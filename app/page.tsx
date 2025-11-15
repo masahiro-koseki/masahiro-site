9708b205026f48e2ccb0ac7972b104a7a6eb00c7
Masahiro Koseki <manbo@MacBook-Air.local>
2025-11-16 02:11:57 +0900

    Change section padding

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const Section = ({ id, children, className = "" }: { id?: string; children: Reac
   
 
   return (
-    <section id={safeId} className={`max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-1 ${extra}`}>
+    <section id={safeId} className={`max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6 ${extra}`}>
       {children}
     </section>
   );
