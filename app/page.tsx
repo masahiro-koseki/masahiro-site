5a0f7a30928992d11c32d5a194c72d85ece8b83b
Masahiro Koseki <manbo@MacBook-Air.local>
2025-11-17 12:24:24 +0900

    Change section-spacing py

--- app/page.tsx
+++ app/page.tsx
@@ -566,7 +566,7 @@ const gallerySources = [
 		</header>
 
 
-      <Section id="home" className="pt-12 pb-12">
+      <Section id="home" className="pt-12 pb-1">
 		<HeroSection texts={t.hero} scrollTo={scrollTo} />
         <div className="w-full h-px bg-neutral-200 my-8"></div>
 	  </Section>
