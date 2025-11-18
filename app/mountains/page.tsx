c0c08dfab3b749d8cb678740e4f4059fc52145f0
Masahiro Koseki <manbo@MacBook-Air.local>
2025-11-18 13:39:55 +0900

    Add mountains page

--- app/mountains/page.tsx
+++ app/mountains/page.tsx
@@ -117,7 +117,7 @@ export default function MountainsPage() {
 					src={mt.image}
 					alt={`${mt.nameJa} / ${mt.nameEn}`}
 					fill
-					className="object-contain"
+					className="object-cover"
 					sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
 					/>
 					</div>
